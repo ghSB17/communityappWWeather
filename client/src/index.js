63a6593a417e079b334d1563e34cdc3fe206c88f
sumathi <sumathi@rocketmail.com>
2018-07-16 18:33:24 -0400

    weather issues

---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// import './styles/app.scss';
+import './styles/_base.css';
+import './styles/_bootstrap.css';
+import './styles/components/Weather/_current-weather-display.css';
+import './styles/components/Weather/_daily-weather-display.css';
+import './styles/components/Weather/_hourly-weather-display.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
